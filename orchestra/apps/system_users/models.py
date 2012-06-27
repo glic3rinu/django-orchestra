5c1a54334d384aa7fcce7f1ce122f685ded68a33
glic3rinu <marcay@pangea.org>
2012-06-27 17:16:33 +0200

    fixed custom_settings

--- orchestra/apps/system_users/models.py
+++ orchestra/apps/system_users/models.py
@@ -5,10 +5,10 @@ import settings
 class SystemUser(models.Model):
     """ /etc/passwd - Users for SuExec and ftp accounts. """ 
     user = models.OneToOneField(User, primary_key=True)
-    shell = models.CharField(max_length=23, default=settings.SYSTEM_USER_DEFAULT_SHELL)
+    shell = models.CharField(max_length=23, default=settings.SYSTEM_USERS_DEFAULT_SHELL)
     uid = models.PositiveIntegerField(unique=True)
-    primary_group = models.ForeignKey('SystemGroup', default=settings.SYSTEM_USER_DEFAULT_PRIMARY_GROUP_PK)
-    homedir = models.CharField(max_length=255, default=settings.SYSTEM_USER_DEFAULT_BASE_HOMEDIR)
+    primary_group = models.ForeignKey('SystemGroup', default=settings.SYSTEM_USERS_DEFAULT_PRIMARY_GROUP_PK)
+    homedir = models.CharField(max_length=255, default=settings.SYSTEM_USERS_DEFAULT_BASE_HOMEDIR)
     only_ftp = models.BooleanField(default=False)
 
     def __unicode__(self):
@@ -18,7 +18,7 @@ class SystemUser(models.Model):
         if not self.uid:
             last = SystemUser.objects.all().order_by('-uid')[0].uid
             if not last:
-                self.uid = settings.SYSTEM_USER_START_UID
+                self.uid = settings.SYSTEM_USERS_START_UID
             else:
                 self.uid = last + 1
         super(SystemUser, self).save(*args, **kwargs)       
--- orchestra/projects/skeleton/orchestra/custom_settings.py
+++ orchestra/projects/skeleton/orchestra/custom_settings.py
@@ -1,43 +1,42 @@
 
 # DNS
-DEFAULT_PRIMARY_NS = 'ns1.pangea.org'
-DEFAULT_HOSTMASTER_EMAIL = 'suport.pangea.org'
-DEFAULT_DOMAIN_REGISTERS = [{'type':'NS', 'data': 'ns1.pangea.org.'},
-                            {'type':'NS', 'data': 'ns2.pangea.org.'},
-                            {'type':'NS', 'data': 'ns3.pangea.org.'},
-                            {'type':'A', 'data': '192.168.0.103'},
-                            {'type':'MX', 'data': '10 mail.pangea.org.'},
-                            {'type':'MX', 'data': '20 mail2.pangea.org.'},
-                            {'name':'www', 'type':'CNAME', 'data': 'web.pangea.org.'},]
-EXTENSIONS = (('org', 'org'),
-              ('net', 'net'),
-              ('es', 'es'),
-              ('cat', 'cat'),
-              ('info', 'info'),)
-DEFAULT_EXTENSION = 'org'
-REGISTER_PROVIDER_CHOICES = (('', 'None'),
-                             ('gandi', 'Gandi'),)
-DEFAULT_REGISTER_PROVIDER = 'gandi'
-DEFAULT_NAME_SERVERS = [{'hostname':'ns1.pangea.org', 'ip': ''},
-                        {'hostname':'ns2.pangea.org', 'ip': ''},
-                        {'hostname':'ns3.pangea.org', 'ip': ''},]
+DNS_DEFAULT_PRIMARY_NS = 'ns1.pangea.org'
+DNS_DEFAULT_HOSTMASTER_EMAIL = 'suport.pangea.org'
+DNS_DEFAULT_DOMAIN_REGISTERS = [{'type':'NS', 'data': 'ns1.pangea.org.'},
+                                {'type':'NS', 'data': 'ns2.pangea.org.'},
+                                {'type':'NS', 'data': 'ns3.pangea.org.'},
+                                {'type':'A', 'data': '192.168.0.103'},
+                                {'type':'MX', 'data': '10 mail.pangea.org.'},
+                                {'type':'MX', 'data': '20 mail2.pangea.org.'},
+                                {'name':'www', 'type':'CNAME', 'data': 'web.pangea.org.'},]
+DNS_EXTENSIONS = (('org', 'org'),
+                  ('net', 'net'),
+                  ('es', 'es'),
+                  ('cat', 'cat'),
+                  ('info', 'info'),)
+DNS_DEFAULT_EXTENSION = 'org'
+DNS_REGISTER_PROVIDER_CHOICES = (('', 'None'),
+                                 ('gandi', 'Gandi'),)
+DNS_DEFAULT_REGISTER_PROVIDER = 'gandi'
+DNS_DEFAULT_NAME_SERVERS = [{'hostname':'ns1.pangea.org', 'ip': ''},
+                            {'hostname':'ns2.pangea.org', 'ip': ''},
+                            {'hostname':'ns3.pangea.org', 'ip': ''},]
 
 # Contacts
-DEFAULT_LANGUAGE = 'ca'
-LANGUAGE_CHOICES = (('ca', 'Catalan'),
-                    ('es', 'Spanish'),
-                    ('en', 'English'),)
+CONTACTS_DEFAULT_LANGUAGE = 'ca'
+CONTACTS_LANGUAGE_CHOICES = (('ca', 'Catalan'),
+                             ('es', 'Spanish'),
+                             ('en', 'English'),)
 
 # Web
-DEFAULT_VIRTUAL_HOST_CUSTOM_DIRECTIVES = '\
+WEB_DEFAULT_VIRTUAL_HOST_CUSTOM_DIRECTIVES = '\
 Alias /webalizer /home/httpd/webalizer/{{ self.unique_ident }}\n\
 ScriptAlias /cgi-bin/ /home/pangea/{{ self.fcgid.user }}/cgi-bin/'
-VIRTUALHOST_IP_CHOICES = (('70.200.179.80', 'web.ucp.org'),)
-VIRTUALHOST_IP_DEFAULT = '70.200.179.80'
+WEB_VIRTUALHOST_IP_CHOICES = (('70.200.179.80', 'web.ucp.org'),)
+WEB_VIRTUALHOST_IP_DEFAULT = '70.200.179.80'
 
 # System_users
-DEFAULT_SYSTEM_USER_BASE_HOMEDIR = '/home/pangea'
-
+SYSTEM_USERS_DEFAULT_BASE_HOMEDIR = '/home/pangea'
 
 # Billing 
 BILLING_INVOICE_ID_PREFIX = 'F'

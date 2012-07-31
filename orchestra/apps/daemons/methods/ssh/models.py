008733c6e631bb47e92ef1fc9a3056f79400a03c
Redmine Admin <admin@confine-project.eu>
2012-07-31 11:57:14 +0200

    fix daemons.method.ssh settings

--- orchestra/apps/daemons/methods/ssh/models.py
+++ orchestra/apps/daemons/methods/ssh/models.py
@@ -3,11 +3,11 @@ import settings
 
 class SSHOption(models.Model):
     host = models.OneToOneField('daemons.Host')
-    user = models.CharField("SSH User", max_length=32, default=settings.DEFAULT_SSH_USER, blank=True)
+    user = models.CharField("SSH User", max_length=32, default=settings.DAEMONS_DEFAULT_SSH_USER, blank=True)
     password = models.CharField("SSH Password", max_length=64, blank=True)
-    host_keys = models.CharField("SSH Host Keys", max_length=255, blank=True, default=settings.DEFAULT_SSH_HOST_KEYS)
+    host_keys = models.CharField("SSH Host Keys", max_length=255, blank=True, default=settings.DAEMONS_DEFAULT_SSH_HOST_KEYS)
     private_key = models.CharField("SSH Private Key", max_length=255, blank=True)
-    port = models.IntegerField("SSH Port", default=settings.DEFAULT_SSH_PORT, blank=True)
+    port = models.IntegerField("SSH Port", default=settings.DAEMONS_DEFAULT_SSH_PORT, blank=True)
     alternate_ip = models.IPAddressField("SSH IP", blank=True, help_text='Default host IP')
     
     def __unicode__(self):

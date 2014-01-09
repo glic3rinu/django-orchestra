25d5064ad6811a213bb43e3ac82157ddca218673
Marc Aymerich <glicerinu@gmail.com>
2014-01-09 11:46:58 +0100

    Update create.sh

--- scripts/container/create.sh
+++ scripts/container/create.sh
@@ -20,8 +20,6 @@ mount --bind /dev $CONTAINER/dev
 mount -t sysfs none $CONTAINER/sys
 trap "umount $CONTAINER/{dev,sys}; exit 1;"INT TERM EXIT
 
-chroot $CONTAINER apt-get install -y --force-yes nano git screen sudo iputils-ping python-pip
-
 echo "root:$PASSWORD" | chroot $CONTAINER chpasswd
 chroot $CONTAINER useradd orchestra -s "/bin/bash"
 echo "$USER:$PASSWORD" | chroot $CONTAINER chpasswd
@@ -30,6 +28,8 @@ chroot $CONTAINER chown $USER.$USER /home/$USER
 chroot $CONTAINER adduser $USER sudo
 
 sed -i "s/\tlocalhost$/\tlocalhost orchestra/" $CONTAINER/etc/hosts
+
+chroot $CONTAINER apt-get install -y --force-yes nano git screen sudo iputils-ping python2.7 python-pip
 
 sleep 0.1
 umount $CONTAINER/{dev,sys}

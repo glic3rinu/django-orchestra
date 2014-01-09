6bfce15ebca50381c1e901e49b22b6741745994d
Marc Aymerich <marcay@pangea.org>
2014-01-09 15:04:47 +0000

    Fixes on orchestra deployment script

--- scripts/container/deploy.sh
+++ scripts/container/deploy.sh
@@ -13,8 +13,10 @@ normal=$(tput sgr0)
 }
 
 USER='orchestra'
-BASE_DIR='/home/orchestra/panel'
-MANAGE="$BASE_DIR/panel/manage.py"
+HOME=$(eval echo "~$USER")
+PROJECT_NAME='panel'
+BASE_DIR="$HOME/$PROJECT_NAME"
+MANAGE="$BASE_DIR/manage.py"
 
 run () {
     echo " ${bold}\$ su $USER -c \"${@}\"${normal}"
@@ -24,47 +26,50 @@ run () {
 
 sudo adduser $USER sudo
 
-CURRENT_VERSION=$(python -c "from orchestra import get_version; print get_version();" || echo false)
+CURRENT_VERSION=$(python -c "from orchestra import get_version; print get_version();" 2> /dev/null || false)
 
 if [[ ! $CURRENT_VERSION ]]; then
     # First Orchestra installation
     run "git clone https://github.com/glic3rinu/django-orchestra.git ~/django-orchestra"
-    echo ~/django-orchestra/ | sudo tee /usr/local/lib/python2.7/dist-packages/orchestra.pth
-    sudo cp ~orchestra/django-orchestra/orchestra/bin/orchestra-admin /usr/local/
+    echo $HOME/django-orchestra/ | sudo tee /usr/local/lib/python2.7/dist-packages/orchestra.pth
+    sudo cp $HOME/django-orchestra/orchestra/bin/orchestra-admin /usr/local/bin/
     sudo orchestra-admin install_requirements
 fi
 
 if [[ ! -e $BASE_DIR ]]; then
-    run "orchestra-admin clone $BASE_DIR"
+    cd $HOME/
+    run "orchestra-admin clone $PROJECT_NAME"
+    cd -
 fi
 
 sudo service postgresql start
-sudo python MANAGE setuppostgres --db_name orchestra --db_user orchestra --db_password orchestra
+sudo python $MANAGE setuppostgres --db_name orchestra --db_user orchestra --db_password orchestra
 
-if [[ $CURRENT_VERSION != false ]]; then
+if [[ $CURRENT_VERSION ]]; then
     # Per version upgrade specific operations
-    sudo python MANAGE postupgradeorchestra --no-restart --from $CURRENT_VERSION
+    sudo python $MANAGE postupgradeorchestra --no-restart --from $CURRENT_VERSION
 else
-    sudo python MANAGE syncdb --noinput
-    sudo python MANAGE migrate --noinput
+    sudo python $MANAGE syncdb --noinput
+    sudo python $MANAGE migrate --noinput
 fi
 
-sudo python MANAGE setupcelery --username $USER --processes 2
+sudo python $MANAGE setupcelery --username $USER --processes 2
 
 # Install and configure Nginx web server
-run "mkdir -p $BASE_DIR/static"
-run "python MANAGE collectstatic --noinput"
+run "mkdir $BASE_DIR/static"
+run "python $MANAGE collectstatic --noinput"
 sudo apt-get install -y nginx uwsgi uwsgi-plugin-python
-sudo python manage.py setupnginx
+sudo python $MANAGE setupnginx
 
 # Apply changes
-sudo python MANAGE restartservices
+sudo python $MANAGE restartservices
 
 # Create a orchestra user
-cat <<- EOF | python MANAGE shell
-	from users.models import *
-	if not User.objects.filter(username='orchestra').exists():
-	    print 'Creating orchestra superuser'
-	    User.objects.create_superuser('orchestra', 'orchestra@localhost', 'orchestra')
-	
+cat <<- EOF | python $MANAGE shell
+    from users.models import *
+    if not User.objects.filter(username='orchestra').exists():
+        print 'Creating orchestra superuser'
+        User.objects.create_superuser('orchestra', 'orchestra@localhost', 'orchestra')
+    
 EOF
+

5ed8773acc85eea1f427731468fb98a4db08a188
Marc <orchestra@orchestra.lan>
2014-09-30 14:52:02 +0000

    Removed old users app

--- orchestra/apps/users/settings.py
+++
@@ -1,6 +0,0 @@
-from django.conf import settings
-
-
-USERS_SYSTEMUSER_HOME = getattr(settings, 'USERES_SYSTEMUSER_HOME', '/home/%(username)s')
-
-USERS_FTP_LOG_PATH = getattr(settings, 'USERS_FTP_LOG_PATH', '/var/log/vsftpd.log')

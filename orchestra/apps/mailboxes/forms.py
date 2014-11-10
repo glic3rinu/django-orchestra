2eb2fb5a5c4838ec3f8368cee48ef154bf53e9e3
Marc Aymerich <glicerinu@gmail.com>
2014-11-10 19:01:24 +0000

    Fixed mailbox form

--- orchestra/apps/mailboxes/forms.py
+++ orchestra/apps/mailboxes/forms.py
@@ -50,10 +50,6 @@ class MailboxForm(forms.ModelForm):
                 'custom_filtering': _("You didn't provide any custom filtering.")
             })
         return custom_filtering
-    
-    def clean(self):
-        if not self.cleaned_data['mailboxes'] and not self.cleaned_data['forward']:
-            raise ValidationError("A mailbox or forward address should be provided.")
 
 
 class MailboxChangeForm(UserChangeForm, MailboxForm):

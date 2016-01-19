9f320e3f5313570578f3fb4e0d780fe11879cf05
Marc Aymerich <glicerinu@gmail.com>
2016-01-19 12:05:42 +0000

    Use 301 sttatus code for http redirects

--- orchestra/contrib/websites/backends/apache.py
+++ orchestra/contrib/websites/backends/apache.py
@@ -319,9 +319,9 @@ class Apache2Backend(ServiceController):
         for redirect in directives.get('redirect', []):
             location, target = redirect.split()
             if re.match(r'^.*[\^\*\$\?\)]+.*$', redirect):
-                redirect = "RedirectMatch %s %s" % (location, target)
+                redirect = "RedirectMatch 301 %s %s" % (location, target)
             else:
-                redirect = "Redirect %s %s" % (location, target)
+                redirect = "Redirect 301 %s %s" % (location, target)
             redirects.append(
                 (location, redirect)
             )
